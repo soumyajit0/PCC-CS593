be0977cafae0e82d8f0c96b16055955c5d2a4ba8
soumyajit0 <soumyajitdeysarkar@gmail.com>
2023-11-29 18:28:24 +0530

    commit

--- Assignment 5/Generic.java
+++ Assignment 5/Generic.java
@@ -25,124 +25,118 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class SimpleStack {
-	public static void main(String args[]) {
-            Scanner sc=new Scanner(System.in);
-	    final int MAX = 5;    /* stack capacity */
-            String[] st=new String[MAX];
-            /* Instantiate GenSimpleStack */		
-            GenSimpleStack<String> stack=new GenSimpleStack<String>(st);
+public class Generic {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        final int MAX = 5; /* stack capacity */
+        String[] st = new String[MAX];
+        /* Instantiate GenSimpleStack */
+        GenSimpleStack<String> stack = new GenSimpleStack<String>(st);
 
-            /* Read choice 0: Quit, 1: Push, 2: Pop */
-            
-            /* Loop until 0 is entered */
-            while (true) {	// Don't forget to return when Quit requested
-                /* Use try-catch */
-                int ch=sc.nextInt();
-                switch(ch)
-                {
-                    case 0:
-                            System.exit(1);
-                    case 1:
-                            try
-                            {
-                                stack.push(sc.next());
-                            }
-                            catch(StackFullException e)
-                            {
-                                System.out.print(e);
-                            }
-                            break;
-                    case 2:
-                            try
-                            {
-                                stack.pop();
-                            }
-                            catch(StackEmptyException e)
-                            {
-                                System.out.print(e);
-                            }
-                            break;
-                    
-                }
-            }    // end of while
-	}    // end of main
+        /* Read choice 0: Quit, 1: Push, 2: Pop */
+
+        /* Loop until 0 is entered */
+        while (true) { // Don't forget to return when Quit requested
+            /* Use try-catch */
+            int ch = sc.nextInt();
+            switch (ch) {
+                case 0:
+                    System.exit(1);
+                case 1:
+                    try {
+                        stack.push(sc.next());
+                    } catch (StackFullException e) {
+                        System.out.print(e);
+                    }
+                    break;
+                case 2:
+                    try {
+                        stack.pop();
+                    } catch (StackEmptyException e) {
+                        System.out.print(e);
+                    }
+                    break;
+
+            }
+        } // end of while
+    } // end of main
 } // End of class SimpleStack
 
 /* Declare interfce IGenSimpleStack<T> */
-interface IGenSimpleStack<T>
-{
+interface IGenSimpleStack<T> {
     public void push(T item) throws StackFullException;
+
     public void pop() throws StackEmptyException;
+
     public boolean isEmpty();
+
     public boolean isFull();
 }
 
-/* Declare GenSimpleStack<T> implementing interface IGenSimpleStack  */
+/* Declare GenSimpleStack<T> implementing interface IGenSimpleStack */
 class GenSimpleStack<T> implements IGenSimpleStack<T> {
-	/* Declare Generic array */
-        T[] data;
-	/* Declare Top of stack */
-	int tos;
-        /* Declare Constructor */
-        GenSimpleStack(T[] ds)
-        {
-            this.data=ds;
-            tos=0;
-        }
-        /* Declare Push */
-        public void push(T item) throws StackFullException
-        {
-            if(isFull()) throw new StackFullException(data.length);
-            data[tos]=item;
-            tos++;
-        }
-        /* Declare Pop */
-        public void pop() throws StackEmptyException
-        {
-            if(isEmpty()) throw new StackEmptyException();
-            tos--;
-            System.out.print(data[tos]+" ");
-        }
-        /* Declare isFull */
-        public boolean isFull()
-        {
-            return tos==data.length;
-        }
-        /* Declare isEmpty */
-        public boolean isEmpty()
-        {
-            return tos==0;
-        }
-}
-/* Declare StackFullException class */
-class StackFullException extends Exception
-{
-    int size;
-    StackFullException(int s)
-    {
-        super("StackFull");
-        size=s;
+    /* Declare Generic array */
+    T[] data;
+    /* Declare Top of stack */
+    int tos;
+
+    /* Declare Constructor */
+    GenSimpleStack(T[] ds) {
+        this.data = ds;
+        tos = 0;
     }
-   
-    /* Override toString() */
-    public String toString()
-    {
-        return ("[Full-5] ");
+
+    /* Declare Push */
+    public void push(T item) throws StackFullException {
+        if (isFull())
+            throw new StackFullException(data.length);
+        data[tos] = item;
+        tos++;
     }
-    
-}
-/* Declare StackEmptyException class */
-class StackEmptyException extends Exception
-{
-    StackEmptyException()
-    {
-        super("StackEmpty");
+
+    /* Declare Pop */
+    public void pop() throws StackEmptyException {
+        if (isEmpty())
+            throw new StackEmptyException();
+        tos--;
+        System.out.print(data[tos] + " ");
     }
-    /* Override toString() */
-    public String toString()
-    {
-        return ("[Empty] ");
+
+    /* Declare isFull */
+    public boolean isFull() {
+        return tos == data.length;
+    }
+
+    /* Declare isEmpty */
+    public boolean isEmpty() {
+        return tos == 0;
     }
 }
 
+/* Declare StackFullException class */
+class StackFullException extends Exception {
+    int size;
+
+    StackFullException(int s) {
+        super("StackFull");
+        size = s;
+    }
+
+    /* Override toString() */
+    public String toString() {
+        return ("[Full-5] ");
+    }
+
+}
+
+/* Declare StackEmptyException class */
+class StackEmptyException extends Exception {
+    StackEmptyException() {
+        super("StackEmpty");
+    }
+
+    /* Override toString() */
+    public String toString() {
+        return ("[Empty] ");
+    }
+}
